be5d1885ff2f19f5b0655af804b7a057265f3763
Wilply <clement.lecoq@protonmail.ch>
2019-08-24 15:52:45 +0200

    apt error

--- scripts/telegraf_install.bash
+++ scripts/telegraf_install.bash
@@ -10,7 +10,7 @@ wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 echo "deb https://repos.influxdata.com/debian ${VERSION} stable | sudo tee /etc/apt/sources.list.d/influxdb.list"
 
 echo "[INFO] installing telegraf & tools"
-sudo apt-get update && sudo atp-get install -y hddtemps lm-sensors telegraf
+sudo apt-get update && sudo apt-get install -y hddtemps lm-sensors telegraf
 
 echo "[INFO] getting config file"
 sudo wget -qO- https://raw.githubusercontent.com/Wilply/compose-files/master/04_grafana/telegraf.conf > /etc/telegraf/telegraf.conf
